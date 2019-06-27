0efbd888ebc5e050329290181b751c28ad75c3ae
pilatae66 <pilatae66@gmail.com>
2019-06-27 12:15:49 +0800

    minor addition to initialization

--- Initialize.bat
+++ Initialize.bat
@@ -9,7 +9,11 @@ ECHO Copying .env file
 CALL copy .env.example .env
 ECHO generating application key...
 CALL php artisan key:generate
+echo Creating Database, migrating tables and seeding default data
 call php artisan migrate:fresh --seed
 ECHO Initialization done!
+cd vendor/laravel/framework/src/Illuminate/Foundation/Auth/
+start notepad AuthenticatesUsers.php
+cd ../../../../../../..
 start CCSSMS.bat
 PAUSE
