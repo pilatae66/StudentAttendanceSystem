d09dbe98cc88175ce3ee3b916aebc958c00a0078
pilatae66 <pilatae66@gmail.com>
2019-06-27 11:47:28 +0800

    minor fixes

--- CCSSMS.bat
+++ CCSSMS.bat
@@ -1,7 +1,7 @@
-@ECHO OFF
-TITLE APPLICATION INITIALIZATOR
+@echo off
+TITLE APPLICATION OPENER
 ECHO Application Process Initiated
+START http://localhost:8000
 ECHO Starting Server...
-php artisan serve
-START "" http://localhost:8000
+call php artisan serve
 @PAUSE
